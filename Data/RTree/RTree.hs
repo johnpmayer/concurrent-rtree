da9615af8bc88c7aad754d209b0e35e22e4e2b5a
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2014-07-15 18:17:22 -0400

    got arbitrary depth tree/root stuff working

--- Data/RTree/RTree.hs
+++ Data/RTree/RTree.hs
@@ -1,30 +1,38 @@
 
 {-# OPTIONS_GHC -Wall #-}
-{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ExistentialQuantification #-}
 
 module Data.RTree.RTree where
 
-import Control.Applicative
 import Control.Concurrent.STM
+import qualified Data.Vector as V
 
 import qualified Data.RTree.Internal.RTree as I
 import Data.RTree.Internal.RTree (RInsert(..))
 import Data.RTree.TStore
 
-data RTree a = forall h. RTree
-  { config :: I.RConfig
-  , root :: I.RTree h a
+data RRoot a = forall h. Root (I.RTree h a)
+
+data RTree a = Tree
+  { getConfig :: I.RConfig
+  , getRootVar :: TVar (RRoot a)
   }
 
 defaultEmpty :: STM (RTree a)
-defaultEmpty = RTree I.defaultConfig <$> I.empty
+defaultEmpty = do
+  n <- I.empty
+  rootVar <- newTVar $ Root n
+  return $ Tree I.defaultConfig rootVar
 
 upsert :: I.R a => KeyT a -> (a -> a) -> RTree a -> STM ()
-upsert key f (RTree cfg node) = do
+upsert key f (Tree cfg rootVar) = do
+  (Root node) <- readTVar rootVar
   result <- I.upsert cfg key f node
   case result of
     NoExpand -> return ()
     Expand _ -> return ()
-    Split _ _ -> error . show $ result
+    Split bt1 bt2 -> do
+      let nodes = V.fromList [bt1, bt2]
+      tNodes <- newTVar nodes
+      writeTVar rootVar $ Root $ I.Node tNodes
